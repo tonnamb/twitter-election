feeb798a153f3ba50efffec18c10e25c593eb81b
srhkddr <sarahkidder18@gmail.com>
2016-04-09 16:56:02 -0400

    user_tweepy.py

---
+++ user_tweepy.py
@@ -0,0 +1,18 @@
+import tweepy
+
+consumer_key = 'bfoN8IIW5YUN5tKD7vRaJJ5aY'
+consumer_secret = 'pZw1UCPbjDQjrTp01IgZL36h4JOo3U0GrokaRUNMHVUVRY6yiq'
+access_token = '1349515753-oGbkzRBurZRAO5AopHY2ZZDbWEtWfeGjqOs6G3X'
+access_token_secret = 'fmXsNFxwF3vQYSdlofPzIUafdCtAQ2yex0rW96HAbsHoh'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+user = api.get_user('sarahhhkidder')
+
+print user.screen_name
+print user.followers_count
+for friend in user.friends():
+	print friend.screen_name
